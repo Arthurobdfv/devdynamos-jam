599563eaedf7eed7e3e83390e098ae3f8f575834
Arthuro Veríssimo <arthurobdfv@gmail.com>
2023-07-01 15:08:02 +0100

    Adicionada a AI do Robo. Mecanicas de pegar e dropar objetos. (Generalizada para ser reutilizada por outros objetos)

--- devdynamos-jam/Assets/Scripts/Actors/ObjetoCarregavel.cs
+++ devdynamos-jam/Assets/Scripts/Actors/ObjetoCarregavel.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjetoCarregavel : MonoBehaviour, ICarregavel
 {
-    public bool SendoCarregado => false;
+    /// <summary>
+    /// Expoe propriedade que diz se este objeto esta sendo carregado
+    /// </summary>
+    public bool BeingCarried => _carryingObject != null;
 
-    void Pegar()
+    /// <summary>
+    /// Expoe o Transform do objeto
+    /// </summary>
+    Transform ICarregavel.CurrentPosition => transform;
+
+    /// <summary>
+    /// Referencia ao objeto que esta carregando este objeto
+    /// </summary>
+    public GameObject _carryingObject;
+    
+    // Metodo para dropar esse item
+    public void OnDrop()
     {
-
+        if (!BeingCarried) return;
+        _carryingObject = null;
+        DropVisualUpdate();
     }
-    void Soltar()
+
+    // Metodo para pegar esse item
+    public void OnPickUp(GameObject obj)
     {
-
+        if (BeingCarried) return;
+        _carryingObject = obj;
+        PickupVisualUpdate();
     }
 
+    public void FixedUpdate()
+    {
+        if (BeingCarried) transform.position = _carryingObject.transform.position;
+    }
+
+    // Matodo usado para fazer as alteracoes visuais quando o objeto e pego
+    private void PickupVisualUpdate()
+    {
+        // Desativar o sprite renderer ou fazer alteracoes visuais ao ser carregado;
+        // Podemos desativar o sprite renderer e ativar um Icone do item em cima do objeto que esta carregando
+    }
+
+    // Metodo usao para fazer as alteracoes visuais quando o objeto e pego
+    private void DropVisualUpdate()
+    {
+        // Reativar o sprite renderer ou fazer alteracoes visuais ao ser largado no chao;
+        // Desativar icone indicando quem esta carregando este item
+    }
 }
