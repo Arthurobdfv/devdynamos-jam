e948ae62e4ea2698aa22f2cab24733a34bc0bda6
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2023-07-01 15:10:45 -0300

    Att

---
+++ devdynamos-jam/Assets/Scripts/Actors/SpawnEnemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float spawnDistance; // distancia do spawn do inimigo
+    [SerializeField] private float spawnFrequency; // Tempo do spawn do inimigo
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+    }
+
+    IEnumerator SpawnEnemyRoutine() // Vai ficar spawnando o inimigo na distancia e no tempo definido
+    {
+        while (true)
+        {
+            Vector2 spawnPosition = (Vector2)transform.position + Random.insideUnitCircle.normalized * spawnDistance;
+            GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(spawnFrequency);
+        }
+    }
+}
