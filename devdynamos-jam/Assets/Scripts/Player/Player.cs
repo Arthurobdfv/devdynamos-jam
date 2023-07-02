8b2faee7708406384bf5bfdd14b0ba4a4dc03f98
Drodde1 <138256889+Drodde1@users.noreply.github.com>
2023-07-01 21:01:06 -0300

    k

--- devdynamos-jam/Assets/Scripts/Player/Player.cs
+++ devdynamos-jam/Assets/Scripts/Player/Player.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rig;
     private Vector2 _direction;
     public float speed;
-  
+
+    public GameObject bulletPrefab;
+    public float bulltetSpeed;
+
+    private Camera playerCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -20,6 +25,11 @@ public class Player : MonoBehaviour
     {
         OnInput();
         OnMove();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
     void OnInput()
     {
@@ -30,5 +40,43 @@ public class Player : MonoBehaviour
     {
         rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
     }
+
+    void Shoot()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            Vector3 targetPosition = hit.point;
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+            Vector3 shootDirection = targetPosition - transform.position;
+            bullet.transform.rotation = Quaternion.LookRotation(shootDirection);
+
+            Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>(); 
+            if(bulletRigidbody != null )
+            {
+                bulletRigidbody.velocity = shootDirection.normalized * bulltetSpeed;
+            }
+        }
+    }
 }
 
+//Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+//RaycastHit hit;
+
+//if (Physics.Raycast(ray, out hit))
+//{
+//    Vector3 targetPosition = hit.point;
+//    GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+//    Vector3 shootDirection = targetPosition - transform.position;
+//    bullet.transform.rotation = Quaternion.LookRotation(shootDirection);
+
+//    Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+//    if (bulletRigidbody != null)
+//    {
+//        bulletRigidbody.velocity = shootDirection.normalized * bulletSpeed;
+//    }
+//}
