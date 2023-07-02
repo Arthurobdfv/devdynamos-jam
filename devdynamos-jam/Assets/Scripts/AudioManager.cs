9dcc9ae755c71381fc27bb77e2bfc3ffff9543cb
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2023-07-02 19:14:42 -0300

    Att

---
+++ devdynamos-jam/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+  public static AudioSource MainAudioSource;
+
+  void Start()
+  {
+    MainAudioSource=GetComponent<AudioSource>();
+  }
+
+  public static void PlayRandomSoundWithRandomPitch(AudioClip[] ac, float minPitch,float maxPitch)
+  {
+    var selectedClip = ac[Random.Range(0,ac.Length)];
+    PlaySoundRandomPitch(selectedClip,minPitch,maxPitch);
+  }
+
+  public static void PlaySoundRandomPitch(AudioClip ac,float minPitch,float maxPitch)
+  {
+    float randomizedPitch = Random.Range(minPitch,maxPitch);
+    PlaySoundPitched(ac,randomizedPitch);
+  }
+
+  public static void PlaySoundPitched(AudioClip ac,float pitch)
+  {
+    MainAudioSource.pitch=pitch;
+    PlaySound(ac);
+  }
+
+  public static void PlayFromRandomClips(AudioClip[] acs)
+  {
+    int randomIndex = Random.Range(0,acs.Length);
+    PlaySound(acs[randomIndex]);
+  }
+
+  public static void PlaySound(AudioClip ac)
+  {
+    MainAudioSource.PlayOneShot(ac);
+    ResetSource();
+  }
+
+  private static void ResetSource()
+  {
+    MainAudioSource.clip=null;
+    MainAudioSource.pitch=1.0f;
+  }
+}
