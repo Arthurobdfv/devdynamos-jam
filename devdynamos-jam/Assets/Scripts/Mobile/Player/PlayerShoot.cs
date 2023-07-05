6fdab37b87509c55d09945e0f389950086820b42
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2023-07-05 07:30:19 -0300

    Att
    Tentando fazer a movimentação da arma

--- devdynamos-jam/Assets/Scripts/Mobile/Player/PlayerShoot.cs
+++ devdynamos-jam/Assets/Scripts/Mobile/Player/PlayerShoot.cs
@@ -3,46 +3,40 @@ using UnityEngine.InputSystem;
 
 public class PlayerShoot : MonoBehaviour
 {
-    private Camera mainCamera;
-    private GameObject player;
-    private InputAction miraAction;
-    private Vector3 lookDirection;
+    private InputAction lookAction;
+    private Vector2 lookInput;
 
-    void Start()
+    public float rotationSpeed = 5f;
+
+    private void OnEnable()
     {
-        mainCamera = Camera.main;
-        player = transform.parent.gameObject;
-        miraAction = new InputAction(binding: "<Gamepad>/rightStick");
-        miraAction.performed += context => SetValorMira(context, context.ReadValue<Quaternion>());
-        miraAction.Enable();
+        lookAction.Enable();
     }
 
-    void Update()
+    private void OnDisable()
     {
-        Vector3 mousePosition = mainCamera.WorldToScreenPoint(player.transform.position) + lookDirection;
-        Vector3 worldMousePosition = mainCamera.ScreenToWorldPoint(mousePosition);
-        lookDirection = worldMousePosition - player.transform.position;
-        lookDirection.z = 0f;
+        lookAction.Disable();
+    }
 
-        if (lookDirection.magnitude > 0.1f)
+    private void Awake()
+    {
+        lookAction = new InputAction("LookAction", binding: "<Gamepad>/rightStick");
+        lookAction.performed += OnLook;
+        lookAction.canceled += OnLook;
+    }
+
+    private void Update()
+    {
+        Vector3 lookDirection = new Vector3(lookInput.x, 0f, lookInput.y);
+        if (lookDirection != Vector3.zero)
         {
-            float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Shoot();
+            Quaternion toRotation = Quaternion.LookRotation(lookDirection);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
     }
 
-    public void SetValorMira(InputAction.CallbackContext context, Quaternion mira)
+    private void OnLook(InputAction.CallbackContext context)
     {
-        lookDirection = mira * Vector3.up;
-    }
-
-    void Shoot()
-    {
-        // Lógica de disparo da arma
+        lookInput = context.ReadValue<Vector2>();
     }
 }
